75ef2d2977544e6030a0b8dbbf8a88e919ed366c
linchian <jean@e-creative.tw>
2020-07-18 21:50:35 +0800

    footerNav

---
+++ components/NavFooter.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-bottom-navigation
+      :value="activeBtn"
+      grow
+      color="blue lighten-1"
+      class="footer-nav"
+    >
+      <v-btn  :to="$i18n.path('')">
+        <span>{{ $t('footer-link.home') }}</span>
+        <v-icon>far fa-home-alt</v-icon>
+      </v-btn>
+      <v-btn  :to="$i18n.path('coupon')">
+        <span>{{ $t('footer-link.coupon') }}</span>
+        <v-icon>far fa-gift fa-2x</v-icon>
+      </v-btn>
+      <v-btn  :to="$i18n.path('service')">
+        <span>{{ $t('footer-link.service') }}</span>
+        <v-icon>far fa-user-headset</v-icon>
+      </v-btn>
+      <v-btn  :to="$i18n.path('my')">
+        <span> {{ $t('footer-link.my') }}</span>
+        <v-icon>far fa-user</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+</template>
+
+<style>
+  .footer-nav .theme--light.v-btn:hover::before{
+    opacity: 0;
+  }
+  .footer-nav{
+    box-shadow:none !important;
+    border-top: 1px solid #F5F5F5;
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,103 +1,47 @@
 <template>
   <v-app>
-    <header class="Header">
-      <div class="container">
-        <h1 class="Header__Title">
-          <logo />
-        </h1>
-        <nav class="Header__Menu">
-          <NuxtLink :to="$i18n.path('')" class="Header__Link" exact>
-            {{ $t('links.home') }}
-          </NuxtLink>
-          <NuxtLink :to="$i18n.path('about')" class="Header__Link" exact>
-            {{ $t('links.about') }}
-          </NuxtLink>
-          <NuxtLink :to="$i18n.path('users')" class="Header__Link" exact>
-            user
-          </NuxtLink>
-          <NuxtLink v-if="$i18n.locale === 'tw'" :to="`/en` + $route.fullPath" class="Header__Link" active-class="none" exact>
-            {{ $t('links.english') }}
-          </NuxtLink>
-          <NuxtLink v-else :to="$route.fullPath.replace(/^\/[^\/]+/, '')" class="Header__Link" active-class="none" exact>
-            {{ $t('links.tw') }}
-          </NuxtLink>
-        </nav>
-      </div>
-    </header>
+    <div>
+      <v-app-bar
+        color="blue lighten-3"
+        dense
+        src="/images/topbg.jpg"
+        elevation="0"
+      >
+        <v-toolbar-title><logo /></v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn class="top-right-btn bg-gradient mx-1" fab dark x-small elevation="0">
+          <v-icon>fas fa-heart</v-icon>
+        </v-btn>
+        <v-btn class="top-right-btn bg-gradient mx-1" fab dark x-small elevation="0">
+          <v-icon>fas fa-comment-alt-dots</v-icon>
+        </v-btn>
+      </v-app-bar>
+    </div>
     <Nuxt />
     <v-footer padless
-              :class="`d-flex justify-space-around`"
-              color="white"
-              app
+        :class="`d-flex justify-space-around`"
+        color="white"
+        app
     >
-
-      <NuxtLink :to="$i18n.path('')" class="footer_link inline-block text-center text-decoration-none grey--text text--darken-1" exact>
-        <i class="far fa-home-alt d-block fa-2x"></i>
-        {{ $t('footer-link.home') }}
-      </NuxtLink>
-      <NuxtLink :to="$i18n.path('coupon')" class="footer_link inline-block" exact>
-        {{ $t('footer-link.coupon') }}
-      </NuxtLink>
-      <NuxtLink :to="$i18n.path('service')" class="footer_link inline-block" exact>
-        {{ $t('footer-link.service') }}
-      </NuxtLink>
-      <NuxtLink :to="$i18n.path('my')" class="footer_link inline-block" exact>
-        {{ $t('footer-link.my') }}
-      </NuxtLink>
+      <NavFooter/>
     </v-footer>
-
-
   </v-app>
 </template>
 
 <script>
   import Logo from '~/components/Logo.vue'
+  import NavFooter from '~/components/NavFooter.vue'
   export default {
     components: {
       Logo,
+      NavFooter
     }
   }
+
 </script>
 
 <style>
 
-  .container {
-    width: 75%;
-    margin: 0 auto;
-  }
-  .container:after {
-    content: "";
-    display: table;
-    clear: both;
-  }
-  .Header {
-    color: #fff;
-    height: 80px;
-    line-height: 80px;
-    background-color: #2e2f30;
-  }
-  .Header__Title {
-    float: left;
-    font-weight: 300;
-    font-size: 30px;
-  }
-  .Header__Menu {
-    float: right;
-  }
-  .Header__Link {
-    font-size: 16px;
-    color: #fff;
-    border: 1px solid #fff;
-    padding: 7px 12px;
-    text-transform: uppercase;
-    text-decoration: none;
-    border-radius: 5px;
-    margin-left: 10px;
-  }
-  .Header__Link:hover {
-    color: #2e2f30;
-    background-color: #fff;
-  }
 
 
 
