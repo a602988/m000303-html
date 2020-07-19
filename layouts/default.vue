3ed89d7d43a190b65a79278ba23da8c2d29933fa
linchian <a680603@gmail.com>
2020-07-19 09:55:03 +0800

    top

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,14 +6,15 @@
         dense
         src="/images/topbg.jpg"
         elevation="0"
+        height="45"
       >
         <v-toolbar-title><logo /></v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn class="top-right-btn bg-gradient mx-1" fab dark x-small elevation="0">
-          <v-icon>fas fa-heart</v-icon>
+        <v-btn class="m-top-right-btn  bg-gradient mx-1" fab dark x-small elevation="0">
+          <v-icon small>fas fa-heart</v-icon>
         </v-btn>
-        <v-btn class="top-right-btn bg-gradient mx-1" fab dark x-small elevation="0">
-          <v-icon>fas fa-comment-alt-dots</v-icon>
+        <v-btn class="m-top-right-btn active bg-gradient mx-1" fab dark x-small elevation="0">
+          <v-icon small dense>fas fa-comment-alt-dots</v-icon>
         </v-btn>
       </v-app-bar>
     </div>
